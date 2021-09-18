98a2ddf6d86a273769541bc19634984f8cdeaaa0
Abshir Ahmed <abshir.ahmed@cazoo.co.uk>
2021-09-18 18:55:40 +0100

    🟢🟢🟢🟢 GREEN -  - to squash

--- src/problem_01_multiples_of_3_or_5/getSumOfMultiples.test.ts
+++ src/problem_01_multiples_of_3_or_5/getSumOfMultiples.test.ts
@@ -1,10 +1,12 @@
-describe('Get Sum of Multiples', () => {
+import { getSumOfAllMultiples } from './getSumOfAllMultiples';
+
+describe('Get Sum of All Multiples', () => {
   it('should return the sum of all multiples of 3 below 10', () => {
     const multiples = [3];
     const limit = 10;
     const expectedSum = 18;
 
-    const sumOfMultiples = getSumOfMultiples(multiples, limit);
+    const sumOfMultiples = getSumOfAllMultiples(multiples, limit);
 
     expect(sumOfMultiples).toEqual(expectedSum);
   });
