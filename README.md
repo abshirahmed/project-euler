64e2b0a0d545f5223df98dc6d0552615f2b330b1
Abshir Ahmed <abshir.ahmed@cazoo.co.uk>
2021-09-18 20:04:40 +0100

    update README

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Project Euler TDD Kata
 
--[ ] Problem 1: [Multiples of 3 or 5](https://projecteuler.net/problem=1)
+-[x] Problem 1: [Multiples of 3 or 5](https://projecteuler.net/problem=1)
+-[ ] Problem 2: [Even Fibonacci numbers](https://projecteuler.net/problem=2)
--- src/problem_01_multiples_of_3_or_5/getSumOfAllMultiplesOfThreeOrFive.test.ts
+++ src/problem_01_multiples_of_3_or_5/getSumOfAllMultiplesOfThreeOrFive.test.ts
@@ -1,12 +1,12 @@
 import { getSumOfAllMultiplesOfThreeOrFive } from './getSumOfAllMultiplesOfThreeOrFive';
 
-describe('Get Sum of All Multiples of Three or Five', () => {
+describe('Get sum of all multiples of three or five', () => {
   it.each`
     limit   | expectedSum
     ${10}   | ${23}
     ${100}  | ${2318}
     ${1000} | ${233168}
-  `('should return $expected below the limit "$limit"', ({ limit, expectedSum }) => {
+  `('should return "$expectedSum" when limit is set to "$limit"', ({ limit, expectedSum }) => {
     const sumOfAllMultiples = getSumOfAllMultiplesOfThreeOrFive(limit);
 
     expect(sumOfAllMultiples).toEqual(expectedSum);
